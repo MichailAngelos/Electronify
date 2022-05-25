fc24d4cc6a289e711d19c76292f21f4d9fb5473f
Michail Angelos Georgiou <michailangelos.georgiou@gmail.com>
2022-05-25 19:44:12 +0300

    seq with EE countries

--- app/controllers/constants/Values.scala
+++ app/controllers/constants/Values.scala
@@ -4,4 +4,34 @@ import scala.util.matching.Regex
 
 object Values {
   val EMAIL_REGEX: Regex = "^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$".r
+
+  val EU_COUNTRIES: Seq[String] = Seq(
+    "Austria",
+    "Belgium",
+    "Bulgaria",
+    "Croatia",
+    "Republic of Cyprus",
+    "Czech Republic",
+    "Denmark",
+    "Estonia",
+    "Finland",
+    "France",
+    "Germany",
+    "Greece",
+    "Hungary",
+    "Ireland",
+    "Italy",
+    "Latvia",
+    "Lithuania",
+    "Luxembourg",
+    "Malta",
+    "Netherlands",
+    "Poland",
+    "Portugal",
+    "Romania",
+    "Slovakia",
+    "Slovenia",
+    "Spain",
+    "Sweden"
+  )
 }
